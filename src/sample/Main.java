6145061b20243e799cd3b448a905db7f1aa026fe
Hassan Alissa <h4ss1501@gmail.com>
2020-04-27 09:41:13 +0200

    with new size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("The Boomerang");
-        primaryStage.setScene(new Scene(root, 800, 675));
+        primaryStage.setScene(new Scene(root, 600, 475));
         primaryStage.show();
     }
 
